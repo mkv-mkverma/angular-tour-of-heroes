84b2cf94ffc5d2ac857eb2f33021245b0756512f
mkv-mkverma <manishverma.cse@gmail.com>
2018-03-16 17:24:40 +0530

    select and display list of heroes

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,10 +13,15 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   }
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero;
+  
   constructor() { }
 
   ngOnInit() {
 
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
